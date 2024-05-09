b7230411694ae497396414710cbe9abfe093f8ac
EMII-SUZUKI <165377089+EMII-SUZUKI@users.noreply.github.com>
2024-05-10 00:33:58 +0900

    Update ComparisonLogicalOperators.java
    インデントを揃えました。

--- src/basicSyntax/ComparisonLogicalOperators.java
+++ src/basicSyntax/ComparisonLogicalOperators.java
@@ -1,36 +1,33 @@
 package basicSyntax;
 
 public class ComparisonLogicalOperators {
-
 	public static void main(String[] args) {
 	   int a = 6;
 	   int b = 3;
 	   
 	   boolean isGreater = a > b;
 	   
-	   System.out.println(isGreater);
+	　 System.out.println(isGreater);
 	   
 	   
 	   boolean isSunny = true;
-       boolean isWarm = true;
+           boolean isWarm = true;
        
-       boolean andResult1 = (isSunny && isWarm);
+           boolean andResult1 = (isSunny && isWarm);
        
-       System.out.println(andResult1);
-       
-       
-       
-       int x = 5;
-       int y = 8;
-       
-       boolean andResult2 = (x >= 0 && y % 2 == 0);
-       
-       System.out.println(andResult2);
+           System.out.println(andResult1);  
        
        
-       boolean hasPermission = false;
+	   int x = 5;
+           int y = 8;
        
-       System.out.println(!hasPermission);
-
+           boolean andResult2 = (x >= 0 && y % 2 == 0);
+           
+	   System.out.println(andResult2);
+       
+       
+           boolean hasPermission = false;
+       
+           System.out.println(!hasPermission);
 	}
 }
